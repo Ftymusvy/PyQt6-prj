ce098d8b103deff02a11ff5c6828f1cf2566e88c
Fatemeh Mousavinejhad <fatemehmosavinejad202@gmail.com>
2024-11-03 23:27:06 +0330

    QMainWindow

---
+++ main.py
@@ -0,0 +1,10 @@
+
+from PyQt6.QtWidgets import QApplication , QWidget,QMainWindow
+import sys
+#QMainWndow
+#QWidget
+#QDialog
+app=QApplication(sys.argv)
+window= QWidget()
+window.show()
+sys.exit(app.exec())
